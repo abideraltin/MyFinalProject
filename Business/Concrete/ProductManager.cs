e1607425d41b69338e9eb38b8c93a460724cb721
MyWorld <MyWorld@DESKTOP-HM25RS2>
2021-02-06 19:46:59 +0300

    7.Hafta

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -17,12 +17,32 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public List<Product> GetAllByCategoryId(int v)
+        {
+            return _productDal.GetAll(p => p.CategoryId == v);
+        }
+
+        private static void NewMethod()
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Product> GetAll()
         {
             //İş Kodları
             //Yetkisi var mı?
 
             return _productDal.GetAll();
+        }
+
+        public List<Product> GetAlllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
         }
     }
 }
--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -8,7 +8,7 @@ namespace Entities.Concrete
     public class Product:IEntity
     {
         public int ProductId { get; set; }
-        public int CateoryId { get; set; }
+        public int CategoryId { get; set; }  
         public string ProductName { get; set; }
         public short UnitsInStock { get; set; }
         public decimal UnitPrice { get; set; }
