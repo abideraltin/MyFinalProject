935ab6d62d4cbc58b4efd5f76f94a2acd1a15c74
MyWorld <MyWorld@DESKTOP-HM25RS2>
2021-02-06 15:31:53 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public int CateoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    }
+}
