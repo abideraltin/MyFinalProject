6d618e19efb9ffc1f04b082597f9984ced310184
MyWorld <MyWorld@DESKTOP-HM25RS2>
2021-02-13 20:11:21 +0300

    10.hafta

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,14 +1,17 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.Dtos;
 using System.Collections.Generic;
 
 namespace Business.Abstract
 {
     public interface IProductService
     {
-        List<Product> GetAll();
-
-        List<Product> GetAlllByCategoryId(int id);
-
-        List<Product> GetByUnitPrice(decimal min, decimal max);
+        IDataResult<List<Product>> GetAll();
+        IDataResult<List<Product>> GetAlllByCategoryId(int id);
+        IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+        IDataResult<Product> GetById(int productId);
+        IResult Add(Product product);
     }
 }
--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,10 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.Dtos;
+using System;
 using System.Collections.Generic;
 
 namespace Business.Concrete
@@ -19,12 +23,14 @@ namespace Business.Concrete
             return _productDal.GetAll(p => p.CategoryID == v);
         }
 
-        public List<Product> GetAll()
+        public IDataResult<List<Product>> GetAll()
         {
-            //İş Kodları
-            //Yetkisi var mı?
+            if (DateTime.Now.Hour==22)
+            {
+                return new ErrorDataResult();
+            }
 
-            return _productDal.GetAll();
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), true,"Ürünler listelendi");
         }
 
         public List<Product> GetAlllByCategoryId(int id)
@@ -35,6 +41,35 @@ namespace Business.Concrete
         public List<Product> GetByUnitPrice(decimal min, decimal max)
         {
             return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Add(Product product) 
+        {
+            //business codes
+
+            if (product.ProductName.Length<2)
+            {
+                //magic string ayrı ayrı yazılması
+                return new ErrorResult(Messages.ProductNameInvalid);
+            }
+            _productDal.Add(product);
+
+            return new SuccessResult(Messages.ProductAdded);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p=>p.ProductID == productId);
+        }
+
+        IDataResult<List<Product>> IProductService.GetAll()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
